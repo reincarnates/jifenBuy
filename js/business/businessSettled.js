e7467c77ea45a973d92b4243cf5f8a6b555a09f8
Your Name <you@example.com>
2019-07-04 11:36:42 +0800

    Signed-off-by: Your Name <you@example.com>

---
+++ js/business/businessSettled.js
@@ -0,0 +1,28 @@
+$(function() {
+
+  $(window).on('scroll', function () {
+    var scrollTop = $(document).scrollTop();
+    if(scrollTop >= 672) {
+      $('.suspension-box').show();
+    }else{
+      $('.suspension-box').hide();
+    }
+  });
+
+  $('#residence').on('click', function() {
+    $('body,html').animate({ scrollTop: 1273 }, 500);
+  });
+
+  // $('#criteria').on('click', function() {
+  //   $('body,html').animate({ scrollTop: 2468 }, 500);
+  // });
+
+  // $('#cooperation').on('click', function() {
+  //   $('body,html').animate({ scrollTop: 3893 }, 500);
+  // });
+
+  $('#cooperativeBrand').on('click', function() {
+    $('body,html').animate({ scrollTop: 2255 }, 500);
+  });
+
+})
